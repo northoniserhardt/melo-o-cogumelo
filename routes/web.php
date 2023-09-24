99d65322389d52a977d7af5b24747a44dc43504b
NorthonEdmond <northon.iserhardt@edmond.com.br>
2023-09-24 11:18:30 -0300

    changing customization route

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::view('/character-customization', 'customize/character');
+Route::view('/', 'customize/character');
 
